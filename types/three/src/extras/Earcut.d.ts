27b5142920f2012f0f9b4d95c12b0453a6e6f845
Suprhimp <aswwwcom@gmail.com>
2023-09-20 17:42:44 +0900

    Update Earcut.d.ts

--- types/three/src/extras/Earcut.d.ts
+++ types/three/src/extras/Earcut.d.ts
@@ -1,4 +1,5 @@
 import { Triangle } from '../Three';
 export namespace Earcut {
+    console.log('hihi');
     function triangulate(data: number[], holeIndices: number[], dim: number): Triangle[];
 }
