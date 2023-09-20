6a7936381ad89e7711fc47a0d42d7a38c56cc64a
Suprhimp <aswwwcom@gmail.com>
2023-09-20 17:42:59 +0900

    Revert "Update Earcut.d.ts"
    This reverts commit 27b5142920f2012f0f9b4d95c12b0453a6e6f845.

--- types/three/src/extras/Earcut.d.ts
+++ types/three/src/extras/Earcut.d.ts
@@ -1,5 +1,4 @@
 import { Triangle } from '../Three';
 export namespace Earcut {
-    console.log('hihi');
     function triangulate(data: number[], holeIndices: number[], dim: number): Triangle[];
 }
